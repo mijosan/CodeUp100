4c9b706885cb8747f5dc631435389053b35cd034
mijosan <mijosan@naver.com>
2020-02-24 18:10:15 +0900

    CodeUp 19번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,20 +1,21 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	public static void main(String[] args) throws NumberFormatException, IOException{	
+	public static void main(String[] args) throws IOException{	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		//StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(),".");
 		
-		String n = br.readLine();
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
 		
-		System.out.println(n);
+		System.out.println(String.format("%04d.%02d.%02d",a,b,c));
+		
    
         br.close();
 	}
