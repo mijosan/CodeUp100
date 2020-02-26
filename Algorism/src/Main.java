4679671f3f626222c8f35473c3b642caf9ff3b4f
mijosan <mijosan@naver.com>
2020-02-26 19:44:52 +0900

    CodeUp 84번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,31 +1,35 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		//StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
-
-		for(int i=1;i<=n;i++) {
-			if(i%3==0) {
-				System.out.print("X");
-			}else {
-				System.out.print(i);
+		int r = Integer.parseInt(st.nextToken());
+		int g = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int sum = 0;
+		for(int i=0;i<r;i++) {
+			for(int j=0;j<g;j++) {
+				for(int k=0;k<b;k++) {
+					bw.write(i+" "+j+" "+k +"\n");
+					sum = sum + 1;
+				}
 			}
-			
-			if(i==n) {
-				
-			}else {
-				System.out.print(" ");
-			}		
 		}
+		bw.write(sum + "");
+
 		
+		bw.close();
 		br.close();
 	}
 }
