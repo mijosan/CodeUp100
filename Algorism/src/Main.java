5ddeb060b5e3c80abe992a66146ecf6969749d14
mijosan <mijosan@naver.com>
2020-02-26 15:43:35 +0900

    CodeUp 81번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -8,21 +8,19 @@ public class Main {
 	public static void main(String[] args) throws IOException{	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		//StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		
-		int i=1;
-		int sum = 0;
-		while(true) {
-			sum = sum + i;
-			i++;
-			
-			if(n<=sum) {
-				System.out.println(i-1);
-				break;
+		for(int i=1;i<=a;i++) {
+			for(int j=1;j<=b;j++) {
+				System.out.println(i + " " + j);
 			}
 		}
+		
+		
+		br.close();
 	}
 }
 
