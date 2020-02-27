380f2aa96e9960308cee7e6f929d5ec0fea41352
mijosan <mijosan@naver.com>
2020-02-27 17:23:11 +0900

    CodeUp 97번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -1,42 +1,51 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+
 import java.util.StringTokenizer;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int[][] arr = new int[20][20];
-		int n = Integer.parseInt(br.readLine());
-		
-		int x;
-		int y;
-		for(int i=0;i<n;i++) {
+		for(int i=1;i<20;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			
-			arr[x][y] = 1; 
-			
+			for(int j=1;j<20;j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
 		
+		int n = Integer.parseInt(br.readLine());
 		
-		for(int i=1;i<arr.length;i++) {
-			for(int j=1;j<arr.length;j++) {
+		for(int i=0;i<n;i++) {
+			StringTokenizer xy = new StringTokenizer(br.readLine());
+			
+			int x = Integer.parseInt(xy.nextToken());
+			int y = Integer.parseInt(xy.nextToken());
+			
+			for(int j=1;j<20;j++) {
+				if(arr[x][j] == 0) {
+					arr[x][j] = 1;
+				}else {
+					arr[x][j] = 0;
+				}
+				
+				if(arr[j][y] == 0) {
+					arr[j][y] = 1;
+				}else {
+					arr[j][y] = 0;
+				}
+			}
+		}	
+		for(int i=1;i<20;i++) {
+			for(int j=1;j<20;j++) {
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
-		}
-
-		
-		
-		
+		}	
 	}
 }
 
