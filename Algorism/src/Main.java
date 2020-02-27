99a59edeff7fcb31f0b2fdd50480cfd7e4600318
mijosan <mijosan@naver.com>
2020-02-27 20:39:12 +0900

    CodeUp 98번

--- Algorism/src/Main.java
+++ Algorism/src/Main.java
@@ -10,42 +10,43 @@ public class Main {
 	public static void main(String[] args) throws IOException{	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int[][] arr = new int[20][20];
-		for(int i=1;i<20;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j=1;j<20;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(br.readLine());
+		int h = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[h+1][w+1];
+		
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
 		
 		for(int i=0;i<n;i++) {
-			StringTokenizer xy = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			
-			int x = Integer.parseInt(xy.nextToken());
-			int y = Integer.parseInt(xy.nextToken());
+			int l = Integer.parseInt(st.nextToken()); //길이
+			int d = Integer.parseInt(st.nextToken()); //방향 0:가로 1:세로
+			int x = Integer.parseInt(st.nextToken()); //x축
+			int y = Integer.parseInt(st.nextToken()); //y축
 			
-			for(int j=1;j<20;j++) {
-				if(arr[x][j] == 0) {
-					arr[x][j] = 1;
-				}else {
-					arr[x][j] = 0;
+			for(int j=0;j<l;j++) {
+				if(d==0) { //가로
+					arr[x][y] = 1;		
+					y++;
+				}else { //세로
+					arr[x][y] = 1;		
+					x++;
 				}
-				
-				if(arr[j][y] == 0) {
-					arr[j][y] = 1;
-				}else {
-					arr[j][y] = 0;
-				}
-			}
-		}	
-		for(int i=1;i<20;i++) {
-			for(int j=1;j<20;j++) {
-				System.out.print(arr[i][j] + " ");
+			}		
+		}
+		
+		for(int i=1;i<h+1;i++) {
+			for(int j=1;j<w+1;j++) {
+				System.out.print(arr[i][j]+" ");
 			}
 			System.out.println();
-		}	
+		}
 	}
 }
 
